5d43d514b3c43b7a01a3953f993d77940144487b
asku1990 <a-p.kvs@pm.me>
2024-05-12 21:16:01 +0300

    User sessions are available and can be removed by the admin.

--- src/components/AdminMenu/UserSessions.js
+++ src/components/AdminMenu/UserSessions.js
@@ -1,18 +1,57 @@
 import React from 'react'
 import styles from './css/UserSessions.module.css'
 
-const UserSessions = ({ userSessions }) => {
+const UserSessions = ({ userSessions, onDelete, onDeleteAll }) => {
   return (
     <div>
-      <h3>User Sessions</h3>
-      {userSessions.map((session) => (
-        <div key={session.session_id} className={styles.session}>
-          <p>Username: {session.username}</p>
-          <p>User Agent: {session.user_agent}</p>
-          <p>Session Start: {session.session_start}</p>
-          <p>Expiration Time: {session.expiration_time}</p>
-        </div>
-      ))}
+      <h3>All user Sessions</h3>
+      <button onClick={() => onDeleteAll('all')}>
+        Delete All User sessions
+      </button>
+      {userSessions.length > 0 ? (
+        userSessions.map((session) => {
+          const sessionStart = new Date(session.session_start)
+          const expirationTime = new Date(session.expiration_time)
+
+          return (
+            <div key={session.session_id} className={styles.session}>
+              <p>
+                <span className={styles.label}>Username:</span>{' '}
+                <span className={styles.value}>{session.username}</span>
+              </p>
+
+              <p>
+                <span className={styles.label}>Session Start Date:</span>{' '}
+                <span className={styles.value}>
+                  {sessionStart.toLocaleDateString()}
+                </span>
+              </p>
+              <p>
+                <span className={styles.label}>Session Start Time:</span>{' '}
+                <span className={styles.value}>
+                  {sessionStart.toLocaleTimeString([], { hour12: false })}
+                </span>
+              </p>
+
+              <p>
+                <span className={styles.label}>Expiration Time:</span>{' '}
+                <span className={styles.value}>
+                  {expirationTime.toLocaleTimeString([], { hour12: false })}
+                </span>
+              </p>
+              <p>
+                <span className={styles.label}>User Agent:</span>{' '}
+                <span className={styles.value}>{session.user_agent}</span>
+              </p>
+              <button onClick={() => onDelete(session.session_id)}>
+                Delete
+              </button>
+            </div>
+          )
+        })
+      ) : (
+        <p className={styles.session}>No User Sessions</p>
+      )}
     </div>
   )
 }
--- src/components/AdminMenu/UserSettings.js
+++ src/components/AdminMenu/UserSettings.js
@@ -6,12 +6,14 @@ import styles from './css/userSettings.module.css'
 import UserDetail from './UserDetail'
 import AddUser from './AddUser'
 import UserSessions from './UserSessions'
+import { use } from 'i18next'
 
 const UserSettings = () => {
   const [selectedSetting, setSelectedSetting] = useState('')
   const [userAccounts, setUserAccounts] = useState([])
   const [selectedUserData, setSelectedUserData] = useState(null)
   const [userSessions, setUserSessions] = useState([]) // Added userSessions state
+  const [userSessionsById, setUserSessionsById] = useState([]) // Added userSessionsById state
   const [error] = useState(null) // Removed setError as it's not being used
 
   useEffect(() => {
@@ -27,6 +29,7 @@ const UserSettings = () => {
     try {
       const response = await api.get('/user') // Replace with your endpoint
       setUserAccounts(response.data)
+      //console.log('User accounts:', userAccounts)
     } catch (error) {
       console.error('Failed to fetch user accounts:', error)
     }
@@ -39,10 +42,19 @@ const UserSettings = () => {
   const fetchUserSessions = async () => {
     try {
       const response = await api.get('/userSessions/get/all') // Replace with your endpoint
-      console.log('User sessions:', response.data)
+      //console.log('User sessions:', response.data)
       setUserSessions(response.data)
     } catch (error) {
       console.error('Failed to fetch user sessions:', error)
+    }
+  }
+
+  const fetchUserSessionsByUserId = async (userId) => {
+    try {
+      const response = await api.get(`/userSessions/get/${userId}`) // Replace with your endpoint
+      setUserSessionsById(response.data)
+    } catch (error) {
+      console.error(`Failed to fetch user sessions for user ${userId}:`, error)
     }
   }
 
@@ -53,36 +65,96 @@ const UserSettings = () => {
       (user) => user.username === event.target.value,
     )
     setSelectedUserData(userData)
+    setUserSessionsById([])
+    fetchUserSessionsByUserId(userData.user_id)
   }
 
   const handleEditUser = (user) => {
-    console.log('Edit user:', user)
+    //console.log('Edit user:', user)
     // Add your logic to edit the user
   }
 
   const handleDeleteUser = async (user) => {
-    console.log('Delete user:', user)
-    try {
-      await api.delete(`/user/delete/${user.user_id}`) // Replace with your endpoint
-      // After deleting, remove the user from the local state
-      setUserAccounts(userAccounts.filter((u) => u.user_id !== user.user_id))
+    if (window.confirm('Are you sure you want to delete this user?')) {
+      //console.log('Delete user:', user)
+      try {
+        await api.delete(`/user/delete/${user.user_id}`) // Replace with your endpoint
+        // After deleting, remove the user from the local state
+        setUserAccounts(userAccounts.filter((u) => u.user_id !== user.user_id))
 
-      // If the deleted user is the currently selected user, clear the selected user data
-      if (selectedUserData && selectedUserData.user_id === user.user_id) {
-        setSelectedUserData(null)
+        // If the deleted user is the currently selected user, clear the selected user data
+        if (selectedUserData && selectedUserData.user_id === user.user_id) {
+          setSelectedUserData(null)
+        }
+
+        // Show a success toast
+        toast.success('User deleted successfully')
+      } catch (error) {
+        console.error('Failed to delete user:', error)
+
+        // Show an error toast
+        toast.error('Failed to delete user')
       }
-
-      // Show a success toast
-      toast.success('User deleted successfully')
-    } catch (error) {
-      console.error('Failed to delete user:', error)
-
-      // Show an error toast
-      toast.error('Failed to delete user')
     }
   }
 
-  console.log('userAccounts:', userAccounts)
+  //console.log('userAccounts:', userAccounts)
+
+  const handleDeleteUserSession = async (sessionId) => {
+    if (window.confirm('Are you sure you want to delete this user session?')) {
+      console.log('Delete session:', sessionId)
+      try {
+        await api.delete(`/userSessions/delete/sessionid/${sessionId}`) // Replace with your endpoint
+        setUserSessions(
+          userSessions.filter((session) => session.session_id !== sessionId),
+        )
+
+        // Show a success toast
+        toast.success('User session deleted successfully')
+      } catch (error) {
+        console.error('Failed to delete user session:', error)
+
+        // Show an error toast
+        toast.error('Failed to delete user session')
+      }
+    }
+  }
+
+  const handleDeleteAllUserSession = async () => {
+    if (window.confirm('Are you sure you want to delete all user sessions?')) {
+      console.log('Delete all user sessions:')
+      try {
+        await api.delete(`/userSessions/delete/all`) // Replace with your endpoint
+        setUserSessions([])
+
+        // Show a success toast
+        toast.success('All user sessions deleted successfully')
+      } catch (error) {
+        console.error('Failed to delete user sessions:', error)
+
+        // Show an error toast
+        toast.error('Failed to delete user sessions')
+      }
+    }
+  }
+
+  const handleDeleteUserSessionByUserId = async (user) => {
+    if (window.confirm('Are you sure you want to delete all user sessions?')) {
+      console.log('Delete all user sessions:')
+      try {
+        await api.delete(`/userSessions/delete/userid/${user.user_id}`) // Replace with your endpoint
+        setUserSessionsById([])
+
+        // Show a success toast
+        toast.success('All user sessions deleted successfully')
+      } catch (error) {
+        console.error('Failed to delete user sessions:', error)
+
+        // Show an error toast
+        toast.error('Failed to delete user sessions')
+      }
+    }
+  }
 
   return (
     <div className={styles.box}>
@@ -97,7 +169,7 @@ const UserSettings = () => {
             Add user account
           </button>
           <button onClick={() => setSelectedSetting('userSessions')}>
-            User sessions
+            All user sessions
           </button>
         </div>
         {selectedSetting === 'addUserAccount' && (
@@ -122,14 +194,20 @@ const UserSettings = () => {
             {selectedUserData && (
               <UserDetail
                 user={selectedUserData}
+                userSessions={userSessionsById}
                 onEdit={handleEditUser}
                 onDelete={handleDeleteUser}
+                onDeleteUsersessions={handleDeleteUserSessionByUserId}
               />
             )}
           </div>
         )}
         {selectedSetting === 'userSessions' && (
-          <UserSessions userSessions={userSessions} />
+          <UserSessions
+            userSessions={userSessions}
+            onDelete={handleDeleteUserSession}
+            onDeleteAll={handleDeleteAllUserSession}
+          />
         )}
       </div>
     </div>
